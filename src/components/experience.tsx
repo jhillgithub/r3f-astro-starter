8064a2b8fec67940024adb12fdfdcde4e9bfc103
jh <>
2023-09-14 14:12:17 -0500

    refactor the scene and experience components

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,8 +1,14 @@
+import { Canvas } from "@react-three/fiber";
 import { Scene } from "./scene";
+import { OrbitControls } from "@react-three/drei";
 export const Experience = () => {
   return (
     <>
-      <Scene />
+      <Canvas>
+        <color attach="background" args={["slategray"]} />
+        <Scene />
+        <OrbitControls />
+      </Canvas>
     </>
   );
 };
--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,17 +1,14 @@
-import { Canvas } from "@react-three/fiber";
-import { Box } from "./box";
 import { Environment } from "@react-three/drei";
+import { Box } from "./box";
 
 export const Scene = () => {
   return (
-    <Canvas>
-      <color attach="background" args={["slategray"]} />
+    <group>
       <Environment files="/background-1k.hdr" />
-
       <ambientLight intensity={0.2} />
       <directionalLight intensity={0.4} position={[10, 10, 10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    </group>
   );
 };
